319fc0613c80a5e81c87f85271e0c2fe02377384
Tomche Delev <tomche.delev@sorsix.com>
2025-08-20 13:35:26 +0200

    add basic repl

---
+++ monkey/lib/repl.ex
@@ -0,0 +1,16 @@
+defmodule Repl do
+
+  def start() do
+    input = IO.gets(">> ") |> String.trim()
+
+    if input != "\\q" do
+      tokens = Lexer.init(input)
+      Enum.each(tokens, fn token -> IO.puts(inspect(token)) end)
+      start()
+    else
+      IO.puts("Goodbye!")
+    end
+  end
+end
+
+Repl.start()
